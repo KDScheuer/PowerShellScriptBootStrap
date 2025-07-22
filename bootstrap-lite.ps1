db07a5402ee699af66da005978684396ef1364e3
KDScheuer <kdscheuer97@gmail.com>
2025-07-21 19:02:12 -0600

    Added Powershell Headers

--- bootstrap-lite.ps1
+++ bootstrap-lite.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+    One sentence describing this scripts purpose.
+.DESCRIPTION
+    More detailed describing scripts purpose.
+.EXAMPLE
+    .\bootstrap.ps1
+    Add more .EXAMPLE statements if this script can be executed in more then one way.
+.INPUTS
+    Any Inputs required for this script. 
+.OUTPUTS
+    Log File to .\logs\bootstrap-logs-yyyyMMdd-T-HHmmss.log
+.NOTES
+    Execution: [Ad-Hoc | Scheduled Task]
+    Compatibility: PowerShell 5.1, and higher
+    Version 1.0.0
+        # Versioning Guide:
+        # - Major Version (X.0.0): Significant changes to the logic, architecture.
+        # - Minor Version (1.X.0): Adding New Features or Improvements.
+        # - Patch Version (1.0.X): Bug Fixes and Logging Changes.
+    Change Log:
+    | Name               | Date       | Version    | Change                                                                                                       |
+    |--------------------|------------|------------|--------------------------------------------------------------------------------------------------------------|
+    | FirstName LastName | YYYY-MM-DD | 1.0.0      | Initial Commit                                                                                               |
+#>
+
 # ==========   Imports   ==========
 # Requires -modules ActiveDirectory
 
